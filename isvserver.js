9146f7698ec23ee36323807a7b265da41dd3fd60
Ignacio Veiga <ignacioveigagiusti@gmail.com>
2022-04-02 16:00:03 -0300

    Desafío: Servidor con Express V1

--- isvserver.js
+++ isvserver.js
@@ -1,8 +1,25 @@
-const express = require('express')
+const express = require('express');
+const app = express();
+const Container = require('./products/container.js');
+const productContainer = new Container('./products/products.json');
 
-const app = express()
+app.get('/', (req, res) => {
+    res.redirect('/products')
+})
+
+app.get('/products', async (req, res) => {
+    const allProducts = await productContainer.getAll();
+    res.send(JSON.stringify(allProducts))
+});
+
+app.get('/randomProduct', async (req, res) => {
+    const allProducts = await productContainer.getAll();
+    let randomId = Math.floor(Math.random() * allProducts.length) +1;
+    const randomProduct = await productContainer.getById(randomId)
+    res.send(JSON.stringify(randomProduct))
+});
 
 const server = app.listen(8080, () => {
-    console.log(`Servidor http inicializado en el puerto ${server.address().port}`)
+    console.log(`Servidor inicializado en el puerto ${server.address().port}`)
 });
-server.on('error', err = console.log(`Error en el servidor: ${err}`))
+server.on("error", err => console.log(`Error en el servidor: ${err}`));
