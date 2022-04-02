c5fe8dccbb53e81210725bc8441ad8e2c84c4abd
Ignacio Veiga <ignacioveigagiusti@gmail.com>
2022-04-02 12:46:20 -0300

    express init and install

--- isvserver.js
+++ isvserver.js
@@ -1,3 +1,8 @@
 const express = require('express')
 
 const app = express()
+
+const server = app.listen(8080, () => {
+    console.log(`Servidor http inicializado en el puerto ${server.address().port}`)
+});
+server.on('error', err = console.log(`Error en el servidor: ${err}`))
